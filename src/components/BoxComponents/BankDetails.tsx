e4117ea3ac868a46c3a194b8c6d57d01b1897af4
Faith Umunnakwe <faithuchinonso@gmail.com>
2022-11-17 01:07:10 +0100

    DIG-35-jobpost-managment

--- src/components/BoxComponents/BankDetails.tsx
+++ src/components/BoxComponents/BankDetails.tsx
@@ -11,71 +11,39 @@ import {
 import ActionMenuBase from "../ActionMenu/ActionMenuBase";
 import ActionMenuItem from "../ActionMenu/ActionMenuItem";
 
-const BankDetails = ({ data: [] }: any) => {
-  const formatData = data
-    ?.slice(0)
-    .reverse()
-    .map((client: any) => {
-      return {
-        abbreviation: client.abbreviation,
-        accountNumber: client.accountNumber,
-        accoutName: client.accoutName,
-        bankName: client.bankName,
-        bankCode: client.bankCode,
-      };
-    });
-  const columnBank = [
-    {
-      Header: "S/N",
-      accessor: "abbreviation",
-      Filter: false,
-    },
-
-    {
-      Header: "Account Number",
-      accessor: "accountNumber",
-    },
-    {
-      Header: "Account Name",
-      accessor: "accoutName",
-    },
-    {
-      Header: "Bank Name",
-      accessor: "bankName",
-    },
-    {
-      Header: "Bank Code",
-      accessor: "bankCode",
-    },
-
-    {
-      Header: "Action",
-      accessor: "action",
-      Filter: false,
-      Cell: (prop: any) => {
-        return (
-          <ActionMenuBase
-            items={
-              <>
-                <ActionMenuItem name="View Details" />
-
-                <ActionMenuItem name="Edit Details" />
-              </>
-            }
-          />
-        );
-      },
-    },
-  ];
+const BankDetails = ({ data }: any) => {
   return (
-    <div>
-      {" "}
-      <MultipleSelectTable
-        columns={columnBank}
-        data={formatData || []}
-        emptyPlaceHolder="No Bank Details yet!"
-        list
-      />
+    <div className="w-[70%]">
+      <div className="flex flex-col items-center">
+        <div className="flex justify-between w-full">
+          <div className="flex flex-col gap-3">
+            <div className="text-xs text-[#8487A3]">Account Name</div>
+            <div className="text-base text-[#090F47]">{data?.accoutName}</div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="text-xs text-[#8487A3]">Account Number</div>
+            <div className="text-base text-[#090F47]">{data?.accoutNumber}</div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="text-xs text-[#8487A3]">Bank Name</div>
+            <div className="text-base text-[#090F47]">{data.bankName}</div>
+          </div>
+        </div>
+        <div className="flex justify-between mt-5 w-full">
+          <div className="flex flex-col gap-3">
+            <div className="text-xs text-text"> Bank Code</div>
+            <div className="text-base text-text">{data.weight}kg</div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="text-xs text-text">Delivery Tag</div>
+            <div className="text-base text-text">{data.bankCode}</div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="text-xs text-text">Abbreviation</div>
+            <div className="text-base text-text">₦ {data.abbreviation}</div>
+          </div>
+        </div>
+      </div>{" "}
     </div>
   );
 };
